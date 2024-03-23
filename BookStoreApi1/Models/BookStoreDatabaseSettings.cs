742ab862eb3c8d6c8cfe0e9309859b11f057d7d5
Luis <luis.angel.larh8586@gmail.com>
2024-03-23 10:17:18 -0600

    Agregar archivos de proyecto.

---
+++ BookStoreApi1/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreApi1.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public String ConnectionString { get; set; } = null!;
+        public String DatabaseName { get; set;} = null!;
+        public String BooksCollectionName { get; set; } = null!;
+
+
+    }
+}
